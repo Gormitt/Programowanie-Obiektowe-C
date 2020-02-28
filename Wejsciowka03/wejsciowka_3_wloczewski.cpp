8381c27a046ab4eadee00e3e8b0dbe666fa6a855
Mateusz Wloczewski <mateusz.wloczewski00@gmail.com>
2020-02-28 19:37:33 +0100

    update

--- Wejsciowka03/wejsciowka_3_wloczewski.cpp
+++ Wejsciowka03/wejsciowka_3_wloczewski.cpp
@@ -17,6 +17,19 @@ struct struktura {
 	int liczba;
 };
 
+/*
+@ brief Funkcja do wypisania informacji o programie.
+*/
+void Info() {
+	printf("autor:\tMateusz Wloczewski\n");
+	printf("data:\t28 lut 2020\n");
+	printf("about: \tWejsciowka na laboratoria 3.\n\n\n");
+}
+
+/*
+@ brief Funkcja do uzupelniania struktury danymi.
+@ param *wsk - wskaznik tablicy struktur struktury
+*/
 void UzupelnijKolekcje(struktura* wsk) {
 	for (int i = 0; i < ILOSC_STRUKTUR; i++) {
 		int liczba = rand() % LICZBA_MAX + 1;
@@ -31,21 +44,54 @@ void UzupelnijKolekcje(struktura* wsk) {
 	}
 }
 
-void WypiszKolekcje	(struktura* wsk) {
+/*
+@ brief Funkcja do wypisania struktury na ekran.
+@ param *wsk - wskaznik tablicy struktur struktury
+*/
+void WypiszKolekcje(struktura* wsk) {
+	printf("Wypisuje kolekcje, czyli tablice struktur: \n");
 	for (int i = 0; i < ILOSC_STRUKTUR; i++) {
 		printf("kolekcja nr. %d: \n", i + 1);
 		printf("nazwa:\t");
 		printf(ANSI_NIEBIESKI "%s\n" ANSI_BIALY, wsk[i].slowo);
 		printf("liczba:\t");
-		printf(ANSI_ZIELONY "%d\n\n" ANSI_BIALY, wsk[i].liczba);
+		printf(ANSI_ZIELONY "%d\n" ANSI_BIALY, wsk[i].liczba);
 	}
+	putchar('\n');
+}
+
+/*
+@ brief Funkcja do skracania napisow w kolekcji struktur.
+        Jezeli napis jest dluzszy niz liczba przechowywana
+		przez strukture, to napisa jest skracany do dlugosci
+		rownej przechowywanej liczbie. W pozostalym wypadku
+		napis jest pozostawiony w niezmienionej formie.
+@ param *wsk - wskaznik do tablicy struktur
+*/
+int SkrocNapisyWKolekcji(struktura* wsk) {
+	int n = 0;
+	for (int i = 0; i < ILOSC_STRUKTUR; i++) {
+		int dlugoscNapisu = strlen(wsk[i].slowo); // pobieram dane o dlugosci slowa i slowie, aby wygodniej sie na nich operowalo
+		int liczba = wsk[i].liczba;
+		if (dlugoscNapisu > wsk[i].liczba) {
+			wsk[i].slowo[liczba] = '\0';
+		}
+	}
+	return n;
 }
 
 int main() {
 	srand(time(NULL));
-	struktura kolekcja[ILOSC_STRUKTUR];
+	struct struktura kolekcja[ILOSC_STRUKTUR];
 
+	Info();
 	UzupelnijKolekcje(kolekcja);
 	WypiszKolekcje(kolekcja);
+	
+	int liczbaSkroconychNapisow = SkrocNapisyWKolekcji(kolekcja);
+	printf("Program wykonal %d skrocen slow w kolekcjach.\n", liczbaSkroconychNapisow);
+	WypiszKolekcje(kolekcja);
+
+	printf("Koniec programu.\n\n");
 	return 0;
 }
